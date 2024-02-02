869a49cdc70372af54975d07442c9799e1c2eed2
Addy-codes <adeeb9839@gmail.com>
2024-02-02 16:13:16 +0530

    Restructured project, added models, views and serializer

--- TaskKeeper/urls.py
+++ TaskKeeper/urls.py
@@ -20,3 +20,13 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+from django.urls import path
+from .views import TodoListView, CustomizeTodoView, BulkAddTodoView, BulkDeleteTodoView
+
+urlpatterns = [
+    path('tasks/', TodoListView.as_view(), name='list-create-tasks'),
+    path('tasks/<int:pk>/', CustomizeTodoView.as_view(), name='retrieve-update-delete-task'),
+    path('tasks/bulk_add/', BulkAddTodoView.as_view(), name='bulk-add-tasks'),
+    path('tasks/bulk_delete/', BulkDeleteTodoView.as_view(), name='bulk-delete-tasks'),
+]
