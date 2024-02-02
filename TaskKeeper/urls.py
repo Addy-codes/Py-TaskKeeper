fe4c337862b95081a4d30207caf50b258e5e4902
Addy-codes <adeeb9839@gmail.com>
2024-02-02 17:12:26 +0530

    Restructuring and adding todo app

--- TaskKeeper/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import TodoListView, CustomizeTodoView, BulkAddTodoView, BulkDeleteTodoView
-
-urlpatterns = [
-    path('tasks/', TodoListView.as_view(), name='list-create-tasks'),
-    path('tasks/<int:pk>/', CustomizeTodoView.as_view(), name='retrieve-update-delete-task'),
-    path('tasks/bulk_add/', BulkAddTodoView.as_view(), name='bulk-add-tasks'),
-    path('tasks/bulk_delete/', BulkDeleteTodoView.as_view(), name='bulk-delete-tasks'),
-]
