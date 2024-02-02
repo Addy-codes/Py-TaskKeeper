88ee890d54620563f310fd5ee42e4173ae4263f7
Addy-codes <adeeb9839@gmail.com>
2024-02-02 17:49:05 +0530

    bulk delete and bulk add

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TodoListView, CustomizeTodoView
+from .views import TodoListView, CustomizeTodoView, BulkAddTodoView, BulkDeleteTodoView
 
 urlpatterns = [
-    path('tasks/', TodoListView.as_view()),
-    path('tasks/<int:pk>/', CustomizeTodoView.as_view()),
+    path('tasks/', TodoListView.as_view(), name='list-create-tasks'),
+    path('tasks/<int:pk>/', CustomizeTodoView.as_view(), name='retrieve-update-delete-task'),
+    path('tasks/bulk_add/', BulkAddTodoView.as_view(), name='bulk-add-tasks'),
+    path('tasks/bulk_delete/', BulkDeleteTodoView.as_view(), name='bulk-delete-tasks'),
 ]
